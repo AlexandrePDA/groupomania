90f1a9fbf0dcd93af1dd238321deed0b41eab54d
AlexandrePDA <a.pires130@gmail.com>
2022-04-18 18:08:08 +0200

    integration scrolltop in home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Card from '../components/Card';
-import PostOnePost from "../components/PostOnePost"
-import ScrollToTop from "../components/ScrollToTop";
+import Card from "../components/Card";
+import PostOnePost from "../components/PostOnePost";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   // const [connect, setConnect] = useState(false);
 
-//   verfication si connecté ou non
+  //   verfication si connecté ou non
   // const getLocalStorage = localStorage.getItem("token")
   // console.log(getLocalStorage);
 
+  // boutton remonte page
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   useEffect(() => {
-    
     const fetchPosts = async () => {
       const res = await axios.get("http://localhost:3000/api/posts/", {
         headers: {
@@ -24,31 +31,27 @@ const Home = () => {
     };
     fetchPosts();
 
-  //   const haveToken = () => {
-  //     const token = localStorage.getItem('token');
-  //     if(token != null) {
-  //        return setConnect(true)
-  //     } else {
-  //         return setConnect(false)
-  //     }
-  // }
-  // console.log(connect);
-  // haveToken()
+    //   const haveToken = () => {
+    //     const token = localStorage.getItem('token');
+    //     if(token != null) {
+    //        return setConnect(true)
+    //     } else {
+    //         return setConnect(false)
+    //     }
+    // }
+    // console.log(connect);
+    // haveToken()
   }, []);
-  
 
   return (
-  <div>
-
-          <PostOnePost/>
-          {posts.map((item) => 
-            <Card key={item.id} props={item} />
-            )}
-            <ScrollToTop/>
-        
-      
-  </div>
-
-)};
+    <div>
+      <PostOnePost />
+      {posts.map((item) => (
+        <Card key={item.id} props={item} />
+      ))}
+      <BsFillArrowUpCircleFill className="scrollTop" onClick={scrollTop} />
+    </div>
+  );
+};
 
 export default Home;
